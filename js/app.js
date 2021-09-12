ff336f77f73da9bd5e302a2986061dbd41baf2b4
Monirul Islam <devmonir.uiu.cse@gmail.com>
2021-09-13 04:14:37 +0600

    product price problem fixed

--- js/app.js
+++ js/app.js
@@ -37,7 +37,8 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  // changed parseInt to parseFloat
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -46,7 +47,8 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  // fixed the total price and shown 2 digits after decimal
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
